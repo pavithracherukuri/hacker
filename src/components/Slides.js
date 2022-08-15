aeea784306d673fa893dc51866cbe502ed16f638
Admin <admin.push@hackerrank.workspace>
2022-08-15 09:44:06 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,7 +1,41 @@
 import React from 'react';
 
 function Slides({slides}) {
+  const [slideState, changeSlide]=useState(0);
 
+  useEffect(()=> {
+    if(slideState==0){
+      document.getElementById('previous').disabled=true;
+      document.getElementById('restart').disabled=true;
+    }
+  });
+  
+  const restart =()=> {
+    if(slideState !==0){
+      document.getElementById('restart').disabled=true;
+    }
+  };
+  
+  const previous =()=> {
+    if(slideState !==0){
+      changeSlide(slideState - 1);
+      document.getElementById('next').disabled=false;
+    }
+    else{
+      document.getElementById('previous').disabled=true;
+      document.getElementById('next').disabled=false;
+    }
+  };
+  const next=()=> {
+    if(slideState !=slides.length -1) {
+      changeSlide(slideState + 1);
+      document.getElementById('previous').disabled=false;
+      document.getElementById('restart').disabled=false;
+    }
+    else{
+      document.getElementById('next').disabled=true;
+    }
+  };
     return (
         <div>
             <div id="navigation" className="text-center">
@@ -10,8 +44,8 @@ function Slides({slides}) {
                 <button data-testid="button-next" className="small">Next</button>
             </div>
             <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
+                <h1 data-testid="title">{slides[slideState].descp}</h1>
+                <p data-testid="text">{slides[slideState].descp}</p>
             </div>
         </div>
     );
